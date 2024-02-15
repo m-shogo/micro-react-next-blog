d7f5fa1078051ada90909d3e74972b2df3c8d23f
matsuzaki <matsuzaki@codia.co.jp>
2024-02-15 17:44:52 +0900

    domainsからremotePatterns へ

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images:{
-    domains: ['images.microcms-assets.io'],
+  images: {
+    //domains: ['images.microcms-assets.io'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.microcms-assets.io",
+        port: "",
+        pathname: "/assets/m-shogo-cube-blog/**",
+      },
+    ],
   },
 };
 
