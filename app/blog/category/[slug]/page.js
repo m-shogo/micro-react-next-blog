875fc77a7e1fc870afd494e1bff32964d0295ff4
matsuzaki <matsuzaki@codia.co.jp>
2024-02-19 17:47:46 +0900

    getStaticProps（ getServerSideProps ） からの移行

--- app/blog/category/[slug]/page.js
+++ app/blog/category/[slug]/page.js
@@ -1,5 +1,5 @@
 import { getAllCategories, getAllPostsByCategories } from "lib/api";
-import Meta from "components/meta";
+//import Meta from "components/meta";
 import Container from "components/container";
 import PostHeader from "components/post-header";
 import Posts from "components/posts";
@@ -7,44 +7,36 @@ import { getPlaiceholder } from "plaiceholder";
 
 import { eyecatchLocal } from "lib/constants";
 
-export default function Category({ name, posts }) {
+export default async function Category({ params }) {
+  const catSlug = params.slug
+
+  const allCats = await getAllCategories()
+  const cat = allCats.find(({ slug }) => slug === catSlug)
+  const name = cat.name
+
+  const posts = await getAllPostsByCategory(cat.id)
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
+
   return (
     <Container>
-      <Meta pageTitle={name} pageDesc={`${name}に関する記事`} />
       <PostHeader title={name} subtitle="Blog Category" />
       <Posts posts={posts} />
     </Container>
-  );
+  )
 }
 
-export async function getStaticPaths() {
-  const allCats = await getAllCategories();
-  return {
-    paths: allCats.map(({ slug }) => `/blog/category/${slug}`),
-    fallback: false,
-  };
-}
+export const dynamicParams = false
+export async function generateStaticParams() {
+  const allCats = await getAllCategories()
 
-export async function getStaticProps(context) {
-  const catSlug = context.params.slug;
-
-  const allCats = await getAllCategories();
-  const cat = allCats.find(({ slug }) => slug === catSlug);
-
-  const posts = await getAllPostsByCategories(cat.id);
-
-  for (const post of posts) {
-    if (!post.hasOwnProperty("eyecatch")) {
-      post.eyecatch = eyecatchLocal;
-    }
-    const { base64 } = await getPlaiceholder(post.eyecatch.url);
-    post.eyecatch.blurDataURL = base64;
-  }
-
-  return {
-    props: {
-      name: cat.name,
-      posts: posts,
-    },
-  };
+  return allCats.map(({ slug }) => {
+    return { slug: slug }
+  })
 }
