ddf0b874e81a4af13b388bc7866ffa8e68a75b28
matsuzaki <matsuzaki@codia.co.jp>
2024-02-21 12:10:05 +0900

    date出力タイプミス dateIOS→dateISO

--- components/convert-date.js
+++ components/convert-date.js
@@ -1,14 +1,14 @@
 import { parseISO, format } from "date-fns";
 import ja from "date-fns/locale/ja";
 
-export default function ConvertDate({ dateIOS }) {
+export default function ConvertDate({ dateISO }) {
   // if (!dateIOS) {
   //   return null; //  または、適切なデフォルト値を返す
   // }
 
   return (
-    <time dateTime={dateIOS}>
-      {format(parseISO(dateIOS), "yyyy年MM月dd日", {
+    <time dateTime={dateISO}>
+      {format(parseISO(dateISO), "yyyy年MM月dd日", {
         locale: ja,
       })}
     </time>
